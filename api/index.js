9c1a38f4ad1c67e15a0bc91fe86d17a9040aa693
FAMHLY <admin@famhly.com>
2025-09-19 20:32:45 -0400

    Add root route to serve index.html

--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ app.use(session({
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
 app.get('/auth/discord', (req, res) => {
   const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.DISCORD_REDIRECT_URI)}&response_type=code&scope=identify`;
   res.redirect(authUrl);
